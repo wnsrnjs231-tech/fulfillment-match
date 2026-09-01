aab5bee38d0252ed11518b80507b7bee176cee7e
ProductionMonitor <wnsrnjs231@gmail.com>
2026-09-01 20:40:11 +0900

    init: create Spring Boot project

---
+++ src/main/java/com/fulfillment/match/FulfillmentMatchApplication.java
@@ -0,0 +1,12 @@
+package com.fulfillment.match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillmentMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FulfillmentMatchApplication.class, args);
+    }
+}
---
+++ src/test/java/com/fulfillment/match/FulfillmentMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulfillment.match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillmentMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
